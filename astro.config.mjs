858f1c14ebc9d49f5586beb45d7d5efbcc52e31c
suryap.padhi <suryap.padhi@hyscaler.com>
2024-09-12 13:34:41 +0530

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	output: 'server', // Enable SSR
+	// output: 'server', // Enable SSR
 
 	integrations: [mdx(), sitemap()],
 });
