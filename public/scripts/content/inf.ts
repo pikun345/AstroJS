08133b1bc9b86b158563f329b412dc7f7dbda31a
suryap.padhi <suryap.padhi@hyscaler.com>
2024-10-05 15:26:40 +0530

    grouping the elements according to category and tags

--- public/scripts/content/inf.ts
+++ public/scripts/content/inf.ts
@@ -3,6 +3,9 @@
   let newCont:any;
   const Parentjaga:any=document.querySelector(".blog-section");
   let nextSlug=document.querySelector(".blog-section")?.getAttribute("data-nextSlug");
+  let data=document.querySelector(".blog-section")?.getAttribute("data-all");
+  
+
   console.log("nxt slug =",nextSlug);
   const loadNext=async()=>{
     console.log("loading called");
@@ -11,22 +14,16 @@
     loading=true;
     try
     {
-      const res=await fetch(`/blog/${nextSlug}/index.html`);
+      const res=await fetch(`/blog/${nextSlug}`);
       console.log("resp=",res);
       
       if(res.ok){
         const nextPost=await res.text();//recvd html convert to text
-        console.log(nextPost);
         const parser=new DOMParser();
-        console.log("parser = ",parser);
         const newDoc=parser.parseFromString(nextPost,'text/html');//used to convert the text html or xml to actual .html 
-        console.log("new doc",newDoc);
-
-        
         newCont=newDoc.getElementById(`${nextSlug}`);//this is bcoz in new doc entire html present so from the entire html it will fetch only that div where next post started 
         history.pushState(null,'',`/blog/${nextSlug}`)
         nextSlug=newCont.getAttribute("data-nextSlug");
-        console.log("update",nextSlug);
         
         
       
